94b2b8ea6b73c1ce40cce4b63366c2471a6a4a78
Filip Kucera <kucerafilip@gmail.com>
2019-11-10 20:04:02 +0100

    abstract factory design pattern

--- src/main/java/cz/fku/designPatterns/creational/abstractFactory/PizzaIngredientFactory.java
+++ src/main/java/cz/fku/designPatterns/creational/abstractFactory/PizzaIngredientFactory.java
@@ -6,6 +6,9 @@ import cz.fku.designPatterns.creational.abstractFactory.ingredience.Dough;
 import cz.fku.designPatterns.creational.abstractFactory.ingredience.Sauce;
 import cz.fku.effectiveJava.method.Overloading;
 
+// ABSTRACT FACTORY pattern
+// provides an interface for creating families of related or dependent objects
+// without specifying their concrete classes.
 public interface PizzaIngredientFactory {
     Dough createDough();
     Sauce createSauce();
--- src/main/java/cz/fku/designPatterns/creational/abstractFactory/PizzaStore.java
+++ src/main/java/cz/fku/designPatterns/creational/abstractFactory/PizzaStore.java
@@ -1,5 +1,18 @@
 package cz.fku.designPatterns.creational.abstractFactory;
 
 public abstract class PizzaStore {
+    public Pizza orderPizza(String type) {
+        Pizza pizza;
+        pizza = createPizza(type);
+
+        pizza.prepare();;
+        pizza.cut();
+
+        return pizza;
+    }
+
+    // this is the ABSTRACT FACTORY METHOD
+    // defines an interface for creating an object, but lets subclasses decide which
+    // class to instantiate. Factory Method lets a class defer instantiation to subclasses.
     abstract Pizza createPizza(String item);
 }

7d1b7b76182e63350580690a1513edbed1f5646c
Filip Kucera <kucerafilip@gmail.com>
2020-12-31 12:21:57 +0100

    adding coroutines

---
+++ src/main/java/cz/fku/concurrency/java_multithreading/barier/Barrier.java
@@ -0,0 +1,40 @@
+package cz.fku.concurrency.java_multithreading.barier;
+
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Barrier {
+    private final int numberOfWorkers;
+    private Semaphore semaphore = new Semaphore(0);
+    private int counter = 0;
+    private Lock lock = new ReentrantLock();
+
+    public Barrier(int numberOfWorkers) {
+        this.numberOfWorkers = numberOfWorkers;
+    }
+
+    public void barrier() {
+        lock.lock();
+        boolean isLastWorker = false;
+        try {
+            counter++;
+
+            if (counter == numberOfWorkers) {
+                isLastWorker = true;
+            }
+        } finally {
+            lock.unlock();
+        }
+
+        if (isLastWorker) {
+            semaphore.release(numberOfWorkers - 1);
+        } else {
+            try {
+                semaphore.acquire();
+            } catch (InterruptedException e) {
+            }
+        }
+    }
+}
+
---
+++ src/main/java/cz/fku/coroutines/BasicMain.kt
@@ -0,0 +1,15 @@
+package cz.fku.coroutines
+
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+fun main() {
+    val job = GlobalScope.launch { // launch a new coroutine in background and continue
+        delay(1000L) // non-blocking delay for 1 second (default time unit is ms)
+        println("World!") // print after delay
+    }
+    job
+    println("Hello,") // main thread continues while coroutine is delayed
+    Thread.sleep(2000L) // block main thread for 2 seconds to keep JVM alive
+}

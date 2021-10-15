062c00e31a6dd9182c210f142140b202e8606abf
Filip Kucera <kucerafilip@gmail.com>
2021-10-15 09:40:18 +0200

    composite

---
+++ src/main/java/cz/fku/designPatterns/behavioural/composite/Example.java
@@ -0,0 +1,74 @@
+package cz.fku.designPatterns.behavioural.composite;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.*;
+import java.util.function.Consumer;
+
+interface ValueContainer extends Iterable<Integer> {
+    default int sum() {
+        int result = 0;
+        for (Integer v: this) {
+            result = result + v;
+        }
+        return result;
+    }
+}
+
+class SingleValue implements ValueContainer
+{
+    public int value;
+
+    // please leave this constructor as-is
+    public SingleValue(int value)
+    {
+        this.value = value;
+    }
+
+    @NotNull
+    @Override
+    public Iterator<Integer> iterator() {
+        return Collections.singleton(this.value).iterator();
+    }
+
+    @Override
+    public void forEach(Consumer<? super Integer> action) {
+        action.accept(this.value);
+    }
+
+    @Override
+    public Spliterator<Integer> spliterator() {
+        return Collections.singleton(this.value).spliterator();
+    }
+}
+
+class ManyValues extends ArrayList<Integer> implements ValueContainer
+{
+}
+
+
+class MyList extends ArrayList<ValueContainer>
+{
+    // please leave this constructor as-is
+    public MyList(Collection<? extends ValueContainer> c)
+    {
+        super(c);
+    }
+
+    public int sum()
+    {
+        // todo
+        int result = 0;
+        for (ValueContainer v : this ) {
+            result = result + v.sum();
+        }
+        return result;
+    }
+}
+
+
+class Demo {
+    public static void main(String[] args) {
+
+    }
+}

f3f02be8447bf7982f94582f3070a739d5d91acf
Filip Kucera <kucerafilip@gmail.com>
2021-10-17 14:14:55 +0200

    protection proxy

--- src/main/java/cz/fku/designPatterns/behavioural/proxy/Person.java
+++ src/main/java/cz/fku/designPatterns/behavioural/proxy/Person.java
@@ -1,5 +1,13 @@
 package cz.fku.designPatterns.behavioural.proxy;
 
+//interface IPerson {
+//    String drink();
+//
+//    String drive();
+//
+//    String drinkAndDrive();
+//}
+
 class Person
 {
     private int age;
@@ -24,12 +32,50 @@ class Person
     public String drinkAndDrive() { return "driving while drunk"; }
 }
 
-class ResponsiblePerson
+class ResponsiblePerson extends Person
 {
     private Person person;
 
     public ResponsiblePerson(Person person)
     {
+        super(person.getAge());
+        this.person = person;
         // todo
+    }
+
+
+
+    @Override
+    public String drink() {
+        if(getAge() < 18) {
+            return "too young";
+        } else {
+            return super.drink();
+
+        }
+    }
+
+    @Override
+    public String drive() {
+        if(super.getAge() < 16) {
+            return "too young";
+        } else {
+            return super.drive();
+
+        }
+    }
+
+    @Override
+    public String drinkAndDrive() {
+        return "dead";
+    }
+}
+
+//
+
+class Demo {
+    public static void main(String[] args) {
+        ResponsiblePerson responsiblePerson = new ResponsiblePerson(new Person(4));
+        responsiblePerson.drink();
     }
 }
